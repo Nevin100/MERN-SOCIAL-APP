04d204a1e78553d6f86392f9ee8e087910531553
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-11-17 12:06:46 +0530

    new commits

--- Server/index.js
+++ Server/index.js
@@ -9,6 +9,7 @@ import helmet from "helmet";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 /*Configuration*/
@@ -43,6 +44,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 /*Routes*/
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /*Mongoose SetUp*/
 const PORT = process.env.PORT || 6001;
