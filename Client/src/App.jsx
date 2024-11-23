4931a7789f013aa7219c50eac261c85f29449cba
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-11-23 09:26:59 +0530

    new commits

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,16 +3,28 @@ import { Navigate, BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/LoginPage";
 import ProfilePage from "scenes/ProfilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
 
 const App = () => {
+  const mode = useSelector((state) => {
+    state.mode;
+  });
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/homr" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/homr" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
