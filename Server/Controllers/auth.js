6552ce9bd57530930adfc7f20ca95d61ba4912b3
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-05-26 23:10:11 +0530

    Completed project

--- Server/Controllers/auth.js
+++ Server/Controllers/auth.js
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../Models/User.js";
 
-/* REGISTER USER */
+/* REGISTER USER -->*/
 export const register = async (req, res) => {
   try {
     const {
