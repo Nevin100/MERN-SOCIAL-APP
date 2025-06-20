2a739cbf937e03495246fea1f55befc9c1a4c67e
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-06-20 15:51:16 +0530

    Completed And Fixes

--- Client/src/scenes/Widgets/FriendListWidget.jsx
+++ Client/src/scenes/Widgets/FriendListWidget.jsx
@@ -7,6 +7,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from "../states/index.jsx";
 
+// Friends :
 const FriendListWidget = ({ userId }) => {
   const { palette } = useTheme();
   const dispatch = useDispatch();
